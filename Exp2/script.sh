7695c310d5639f07c0205e46795a2614081a6d10
JackWang <jack4shihong@gmail.com>
2023-11-28 14:24:24 +0800

    add exp variable

--- Exp2/script.sh
+++ Exp2/script.sh
@@ -1,16 +1,17 @@
 #! /bin/env bash
 
+exp=Exp2
 dir=$(dirname "$(dirname "$(readlink -f "$0")")")
 
 # 复制workload
-mkdir -p ~/.cache/gem5 && cp "${dir}"/Exp2/riscv-hello ~/.cache/gem5
+mkdir -p ~/.cache/gem5 && cp "${dir}"/${exp}/riscv-hello ~/.cache/gem5
 # 切换assign
 cd "${dir}"/gem5-assignment-template && git checkout assign-0
 # 复制脚本
-cp "${dir}"/Exp2/run.py "${dir}"/gem5-assignment-template
+cp "${dir}"/${exp}/run.py "${dir}"/gem5-assignment-template
 
 # 运行
 gem5.opt "${dir}"/gem5-assignment-template/run.py
 
 # 打包
-tar czvf "${dir}"/Exp2.tar.gz -C "${dir}"/Exp2 .
+tar czvf "${dir}"/${exp}.tar.gz -C "${dir}"/${exp} .
